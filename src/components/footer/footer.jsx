7024d134fe782d5e448f0aecbfb40207e69af07f
iewher <goshagang@mail.ru>
2023-06-20 20:08:03 +0500

    added github/tg icons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "../../style/footer/footer.scss";
 
+import { AiFillGithub } from "react-icons/ai";
+import { BsTelegram } from "react-icons/bs";
+
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-subscribe">
         <h1>Subscribe to our newsletter</h1>
         <p>Read about all the things we do.</p>
+        <div className="footer-subscribe-icons">
+          <a
+            href="https://github.com/iewher"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub />
+          </a>
+          <a
+            href="https://t.me/iewher"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTelegram />
+          </a>
+        </div>
       </div>
       <div className="footer-rights">
         <p>© Aperture Photography, Inc. All rights reserved.</p>

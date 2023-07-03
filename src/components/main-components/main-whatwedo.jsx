0dac9c0f35f8d057bf83168570fb380a8dd169cc
iewher <goshagang@mail.ru>
2023-07-03 22:00:29 +0500

    added popover on button

--- src/components/main-components/main-whatwedo.jsx
+++ src/components/main-components/main-whatwedo.jsx
@@ -5,6 +5,8 @@ import block_2 from "../../img/main/block-2.svg";
 import block_3 from "../../img/main/block-3.svg";
 import block_4 from "../../img/main/block-4.svg";
 
+import { Popover } from "@headlessui/react";
+
 function MainWhatWeDo() {
   return (
     <div className="main-whatwedo">
@@ -18,48 +20,81 @@ function MainWhatWeDo() {
             <img src={block_1} />
             <div className="main-whatwedo-container-blocks-block-text">
               <h1>Product Photography</h1>
-              <p>
-                Cras commodo consequat orci, in convallis risus egestas non.
-                Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus
-                libero ac, cursus quam.
-              </p>
-              <button>Read more</button>
+              <Popover>
+                <Popover.Button>Read more</Popover.Button>
+
+                <Popover.Panel className="absolute z-10">
+                  <div className="grid grid-cols-2">
+                    <p>
+                      {" "}
+                      Cras commodo consequat orci, in convallis risus egestas
+                      non. Nulla efficitur auctor hendrerit. Etiam ut orci
+                      varius, faucibus libero ac, cursus quam.
+                    </p>
+                  </div>
+                </Popover.Panel>
+              </Popover>
             </div>
           </div>
           <div className="main-whatwedo-container-blocks-block">
             <img src={block_2} />
             <div className="main-whatwedo-container-blocks-block-text">
               <h1>Product Photography</h1>
-              <p>
-                Cras commodo consequat orci, in convallis risus egestas non.
-                Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus
-                libero ac, cursus quam.
-              </p>
-              <button>Read more</button>
+              <Popover>
+                <Popover.Button>Read more</Popover.Button>
+
+                <Popover.Panel className="absolute z-10">
+                  <div className="grid grid-cols-2">
+                    <p>
+                      {" "}
+                      Cras commodo consequat orci, in convallis risus egestas
+                      non. Nulla efficitur auctor hendrerit. Etiam ut orci
+                      varius, faucibus libero ac, cursus quam.
+                    </p>
+                  </div>
+                </Popover.Panel>
+              </Popover>
             </div>
           </div>
           <div className="main-whatwedo-container-blocks-block">
             <img src={block_3} />
             <div className="main-whatwedo-container-blocks-block-text">
               <h1>Product Photography</h1>
-              <p>
-                Cras commodo consequat orci, in convallis risus egestas non.
-                Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus
-                libero ac, cursus quam.
-              </p>
-              <button>Read more</button>
+
+              <Popover>
+                <Popover.Button>Read more</Popover.Button>
+
+                <Popover.Panel className="absolute z-10">
+                  <div className="grid grid-cols-2">
+                    <p>
+                      {" "}
+                      Cras commodo consequat orci, in convallis risus egestas
+                      non. Nulla efficitur auctor hendrerit. Etiam ut orci
+                      varius, faucibus libero ac, cursus quam.
+                    </p>
+                  </div>
+                </Popover.Panel>
+              </Popover>
             </div>
           </div>
           <div className="main-whatwedo-container-blocks-block">
             <img src={block_4} />
             <div className="main-whatwedo-container-blocks-block-text">
               <h1>Product Photography</h1>
-              <p>
-                Cras commodo consequat orci, in convallis risus egestas non.
-                Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus
-                libero ac, cursus quam.
-              </p>
-              <button>Read more</button>
+              <Popover>
+                <Popover.Button>Read more</Popover.Button>
+
+                <Popover.Panel className="absolute z-10">
+                  <div className="grid grid-cols-2">
+                    <p>
+                      {" "}
+                      Cras commodo consequat orci, in convallis risus egestas
+                      non. Nulla efficitur auctor hendrerit. Etiam ut orci
+                      varius, faucibus libero ac, cursus quam.
+                    </p>
+                  </div>
+                </Popover.Panel>
+              </Popover>
             </div>
           </div>
         </div>

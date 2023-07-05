c9caff521020bd3a8a3feb468a64bd29f0709a32
iewher <goshagang@mail.ru>
2023-07-05 10:24:36 +0500

    fixed footer/added popover

--- src/components/main-components/main-tools.jsx
+++ src/components/main-components/main-tools.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import tools from "../../img/main/main-tools.svg";
 
+import { Popover } from "@headlessui/react";
+
 function MainTools() {
   return (
     <div className="main-tools">
@@ -14,7 +16,21 @@ function MainTools() {
           lot) of film these days — these are the tools that we actually use
           everyday to capture the amazing things around us.
         </p>
-        <button>Check it out</button>
+        <Popover>
+          <Popover.Button>Check it out</Popover.Button>
+
+          <Popover.Panel className="absolute z-10">
+            <div className="grid grid-cols-2">
+              <p>
+                {" "}
+                The say that "no place is boring if you've had a good night's
+                sleep and have a pocket full of unexposed film". While we don't
+                shoot (a lot) of film these days — these are the tools that we
+                actually use everyday to capture the amazing things around us.
+              </p>
+            </div>
+          </Popover.Panel>
+        </Popover>
       </div>
       <div className="main-tools-right">
         <img src={tools} />
